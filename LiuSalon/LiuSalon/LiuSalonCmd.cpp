e8538fabac290a9cdf672b54da197f28596579f1
Fannie <fanniefliu@gmail.com>
2013-02-28 10:02:06 -0500

    adding node and cmd files

--- LiuSalon/LiuSalon/LiuSalonCmd.cpp
+++ LiuSalon/LiuSalon/LiuSalonCmd.cpp
@@ -1,45 +1,33 @@
-//
-// Copyright (C) 
-// 
-// File: LiuSalonCmd.cpp
-//
-// MEL Command: LiuSalon
-//
-// Author: Maya Plug-in Wizard 2.0.1
-// Revised by Isaac Peral
-//
+#include "LiuSalonCmd.h"
 
-// Includes everything needed to register a simple MEL command with Maya.
-// 
 #include <maya/MSimple.h>
 
-// Use helper macro to register a command with Maya.  It creates and
-// registers a command that does not support undo or redo.  The 
-// created class derives off of MPxCommand.
-//
-DeclareSimpleCommand( LiuSalon, "", "2012");
+//DeclareSimpleCommand( LiuSalon, "", "2012");
 
-MStatus LiuSalon::doIt( const MArgList& args )
-//
-//	Description:
-//		implements the MEL LiuSalon command.
-//
-//	Arguments:
-//		args - the argument list that was passes to the command from MEL
-//
-//	Return Value:
-//		MS::kSuccess - command succeeded
-//		MS::kFailure - command failed (returning this value will cause the 
-//                     MEL script that is being run to terminate unless the
-//                     error is caught using a "catch" statement.
-//
+// command line args
+// TODO: fill in
+
+LiuSalonCmd::LiuSalonCmd() : MPxCommand()
+{
+}
+
+LiuSalonCmd::~LiuSalonCmd() 
+{
+}
+
+MSyntax LiuSalonCmd::newSyntax()
+{
+    MSyntax syntax;
+
+    // TODO: fill in
+
+	return syntax;
+}
+
+MStatus LiuSalonCmd::doIt( const MArgList& args )
 {
 	MStatus stat = MS::kSuccess;
-
-	// Since this class is derived off of MPxCommand, you can use the 
-	// inherited methods to return values and set error messages
-	//
-	setResult( "LiuSalon command executed!\n" );
+	//setResult( "LiuSalon command executed!\n" );
 
 	return stat;
 }
